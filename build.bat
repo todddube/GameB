8cbefb805b0240e31f044bbb4c5fd85fdc41219f
Todd Dube <todddube@users.noreply.github.com>
2024-07-06 09:11:16 -0400

    working fork

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ set CompilerDebug=-nologo -Zi -MTd
 set Linker=-nologo -link -subsystem:windows -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib advapi32.lib ole32.lib
 
 pushd build
-cl %CompilerDebug% -LD  ../GameCode/Main.c  %Linker% -EXPORT:TestFunc01 -OUT:GameCode.dll
-cl %CompilerDebug% ../Main.c ../Battle.c ../CharacterNamingScreen.c ../ExitYesNoScreen.c  ../GamepadUnplugged.c ../miniz.c ../NewGameAreYouSure.c ../OpeningSplashScreen.c ../OptionsScreen.c ../Overworld.c ../stb_vorbis.c ../TitleScreen.c  %Linker%
+cl %CompilerDebug% -LD  ../GameCode/Main.c  %Linker% -OUT:GameCode.dll
+cl %CompilerDebug% ../GameCode/Main.c ../Battle.c ../CharacterNamingScreen.c ../ExitYesNoScreen.c  ../GamepadUnplugged.c ../miniz.c ../NewGameAreYouSure.c ../OpeningSplashScreen.c ../OptionsScreen.c ../Overworld.c ../stb_vorbis.c ../TitleScreen.c  %Linker%
 del *.obj
 popd
 CopyAssets.bat
